3ea04d9da61827c3125f6c6954c6a2500dba10d4
Marc Päpper <marc.paepper@mindpeak.ai>
2020-07-02 12:38:43 +0200

    Updated lazys.sh

--- .custom/bin/lazys.sh
+++ .custom/bin/lazys.sh
@@ -3,7 +3,7 @@
 configFile=~/.lazyssh
 
 if [ "$1" == "--help" ]; then
-  echo "Call with 'create \$key \$x@y \$port (opt)' to create a new entry.
+    echo "Call with 'create \$key \$x@y \$port (opt) \$privatekey (opt)' to create a new entry.
 Call with copy 'from' 'to' to copy from server to here. Call with copyto 'to' 'from' to to copy from here to server."
   exit 1
 elif [ "$1" == "create" ]; then
@@ -16,6 +16,11 @@ elif [ "$1" == "create" ]; then
   else
     port="22"
   fi
+  if [ "$#" == 5 ]; then
+    private=$5
+  else
+    private="id_rsa"
+  fi
   if [ ! -f $configFile ]; then
     touch $configFile
     echo -e "declare -A ips\ndeclare -A names\ndeclare -A ports\n" > $configFile
@@ -23,6 +28,7 @@ elif [ "$1" == "create" ]; then
   echo -e "\nnames[$key]=\"$name\"" >> $configFile
   echo -e "ips[$key]=\"$server\"" >> $configFile
   echo -e "ports[$key]=\"$port\"" >> $configFile
+  echo -e "keys[$key]=\"$private\"" >> $configFile
   echo "The new key was created."
   exit 1
 else
@@ -31,7 +37,7 @@ else
   fi
 
   if [ ${#ips[@]} -eq 0 ]; then
-    echo "No configuration found so far. Start creating configuration settings by running this command using 'lazys.sh create \$connectionKey \$name@\$server \$port'"
+    echo "No configuration found so far. Start creating configuration settings by running this command using 'lazys.sh create \$connectionKey \$name@\$server \$port \$privatekey'"
     exit 1
   fi
  
@@ -48,15 +54,16 @@ fi
 ip=${ips[$choice]}
 name=${names[$choice]}
 port=${ports[$choice]}
+key=${keys[$choice]}
 
 if [ "$1" == "cp" ]; then
-  echo "Will do: scp -P$port -r $name@$ip:$2 $3"
-  scp -P$port -r $name@$ip:$2 $3
+  echo "Will do: scp -ip ~/.ssh/$key -P$port -r $name@$ip:$2 $3"
+  scp -p -i ~/.ssh/$key -P$port -r $name@$ip:$2 $3
 elif [ "$1" == "cpto" ]; then
-  echo "Will do: scp -P$port -r $2 $name@$ip:$3"
-  scp -P$port -r $2 $name@$ip:$3
+  echo "Will do: scp -ip ~/.ssh/$key -P$port -r $2 $name@$ip:$3"
+  scp -p -i ~/.ssh/$key -P$port -r $2 $name@$ip:$3
 else
-  echo "Will do: ssh -p$port $name@$ip"
-  ssh -p$port $name@$ip
+  echo "Will do: ssh -i ~/.ssh/$key -p$port $name@$ip"
+  ssh -i ~/.ssh/$key -p$port $name@$ip
 fi
 
